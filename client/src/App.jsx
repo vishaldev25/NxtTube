d151e80b947d9107951a6e250a96af0790fc9e86
VISHAL <vish2512d@gmail.com>
2025-05-16 19:08:33 +0530

    Collected all required assests and exported. Setting the icon for the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 
 const App = () => {
   return (
     <div>
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="font-extrabold text-blue-800 text-7xl">
       Hello world!
     </h1>
     </div>
---
+++ client/src/assets/index.js
@@ -0,0 +1,29 @@
+import banner_bg from "./nxt_watch_banner_bg.png"
+import facebook_logo from "./nxt_watch_facebook_logo.png"
+import failure_view_dark_theme from "./nxt_watch_failure_view_dark_theme.png"
+import failure_view_light_theme from "./nxt_watch_failure_view_light_theme.png"
+import linkedin_logo from "./nxt_watch_linkedin_logo.png"
+import nxtwatch_logo_dark from "./nxt_watch_logo_dark_theme.png"
+import nxtwatch_logo_light from "./nxt_watch_logo_light_theme.png"
+import no_saved_videos from "./nxt_watch_no_saved_videos.png"
+import no_search_results from "./nxt_watch_no_search_results.png"
+import not_found_dark from "./nxt_watch_not_found_dark_theme.png"
+import not_found_light from "./nxt_watch_not_found_light_theme.png"
+import profile from "./nxt_watch_profile_img.png"
+import twitter_logo from "./nxt_watch_twitter_logo.png"
+
+export {
+    banner_bg,
+    facebook_logo,
+    failure_view_dark_theme,
+    failure_view_light_theme,
+    twitter_logo,
+    linkedin_logo,
+    nxtwatch_logo_dark,
+    nxtwatch_logo_light,
+    no_saved_videos,
+    no_search_results,
+    not_found_dark,
+    not_found_light,
+    profile
+}
