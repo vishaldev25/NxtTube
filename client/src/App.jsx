b2fa0ced6ca8b1d55c0238d5353b8d96575a5f05
VISHAL <vish2512d@gmail.com>
2025-05-18 12:14:37 +0530

    Creating a protected Route, which allows the authenticated users to visit the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Link, Routes, Route, Navigate } from "react-router-dom"
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import ProtectedRoute from "./Routes/ProtectedRoute"
 import {
   Home, Gaming, SavedVideos, VideoItemDetails,
   Login, Trending, NotFound
@@ -11,14 +11,16 @@ const App = () =>
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoute />}>
           <Route path="/" element={<Home />} />
           <Route path="/videos/:id" element={<VideoItemDetails />} />
           <Route path="/trending" element={<Trending />} />
           <Route path="/gaming" element={<Gaming />} />
           <Route path="/saved-videos" element={<SavedVideos />} />
-          <Route path="*" element={<Navigate to="/not-found" replace />} />
           <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/not-found" replace />} />
+        </Route>
         </Routes>
       
     </BrowserRouter>
