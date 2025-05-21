484f58402383e8e82c378ae5037ccddb6eec0454
VISHAL <vish2512d@gmail.com>
2025-05-21 20:47:26 +0530

    Upadte it in the main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
+import { SavedVideosProvider } from './context/SavedVideosContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <SavedVideosProvider>
+        <App />
+      </SavedVideosProvider>
       </ThemeProvider>
   </StrictMode>,
 )
